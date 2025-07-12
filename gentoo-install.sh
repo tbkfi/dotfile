0ba27951bcde9ed9100e23b4e686b3e0e30e082d
Tuomo Björk <tuomo.bjork@tbk.fi>
2025-07-12 03:40:49 +0300

    update: gentoo-install.sh (small cleanup)

--- gentoo-install.sh
+++ gentoo-install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-GENTOO_PROFILE="default/linux/amd64/23.0/no-multilib/hardened/systemd"
-TIMEZONE="Europe/Helsinki"
+# Tuomo Björk
+# 2025-06-13
 
-THREAD_COUNT=""		# Set by helper
-LOAD_MAX=""		# Set by helper
-MEMORY_TOTAL=""		# Set by helper
+# System settings
+DEP_GENTOO_PROFILE="default/linux/amd64/23.0/no-multilib/hardened/systemd"
+DEP_TIMEZONE="Europe/Helsinki"
+DEP_HOSTNAME="gentoo"
+
+# Credentials for the new user
+DEP_USER_NAME="user"
+DEP_USER_PASS="123"
+DEP_USER_SHELL="/bin/bash"
 
 
 gentoo_install() {
@@ -22,36 +28,37 @@ gentoo_install() {
 	env-update && source /etc/profile
 
 	# Portage
-	rm -rf /etc/portage/package.{use,license,mask,unmask,accept_keyword}
-	mkdir /etc/portage/package.{use,license,mask,unmask,accept_keyword}
-	cp $HOME/dotfile/src/sys/etc/portage/make.conf /etc/portage/make.conf
+	if [[ -f "$PATH_DOTFILE/src/user/bin/portage-refresh" ]]; then
+		source "$PATH_DOTFILE/src/bin/portage-refresh"
+		portage_refresh
+		if (( $? )); then
+			echo "portage-refresh returned non-zero"
+			exit 1
+		fi
+	else
+		echo "missing 'portage-refresh' script"
+		exit 1
+	fi
 
-	emerge -uDN app-portage/cpuid2cpuflags
-	echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags # https://wiki.gentoo.org/wiki/CPU_FLAGS_*
-
-	portage_refresh
-	portage_build_load
-
-	# Update system
-	# note: do in steps, helps to get closer before manual intervention
-	# is required if something is broken (or avoids issues: e.g. go-bootstrap).
+	# Update system (brrr)
+	# note: do in steps, gets closer to finish before issues crop up.
 	local SETS_LS="$(ls /etc/portage/sets)"
 	local SETS=$(printf "@%s " $SETS_LS)
 
 	emerge -vuDN sys-kernel/linux-firmware
 	echo "emerging: $SETS"
-	emerge -avuDN @world
-	emerge -avuDN @dot-base
-	emerge -avuDN @dot-liblang
-	emerge -avuDN @dot-utility
-	emerge -avuDN @dot-desktop
-	emerge -avuDN @dot-user
-	emerge -avuDN $SETS
+	emerge -vuDN @world
+	emerge -vuDN @dot-base
+	emerge -vuDN @dot-liblang
+	emerge -vuDN @dot-utility
+	emerge -vuDN @dot-desktop
+	emerge -vuDN @dot-user
+	emerge -vuDN @world $SETS
 	if (( $? )); then
-		echo "Fix errors, retry."
+		echo -e "\nFix errors, retry."
 		exit 1
 	fi
-	emerge -av --depclean
+	emerge -v --depclean
 	echo ""
 }
 
@@ -60,17 +67,10 @@ gentoo_boot() {
 	local UUID_ESP=$(blkid --match-token PARTLABEL=ESP --match-tag UUID | awk -F'"' '{print $2}')
 	local UUID_LUKS=$(blkid --match-token PARTLABEL=LUKS --match-tag UUID | awk -F'"' '{print $2}')
 	local UUID_ROOT=$(blkid --match-token LABEL=gentoo --match-tag UUID | awk -F'"' '{print $2}')
-	local TMP_SIZE=0
-
-	# Condition for mounting /tmp in ram
-	if (( $MEMORY_TOTAL - $THREAD_COUNT * 2 >= 32 )); then
-		TMP_SIZE=32
-		echo "Enough ram for /tmp"
-	fi
 
 	# Boot manager
 	bootctl --path=/efi install
-	emerge --config gentoo-kernel-bin
+	emerge --config gentoo-kernel
 
 	# Initramfs
 	mkdir -p /etc/dracut.conf.d
@@ -80,7 +80,7 @@ gentoo_boot() {
 
 	# Kernel CMDLINE
 	mkdir -p /etc/kernel
-	echo "rd.luks.uuid=$UUID_LUKS root=UUID=$UUID_ROOT" > /etc/kernel/cmdline
+	echo "rd.luks.uuid=$UUID_LUKS root=UUID=$UUID_ROOT quiet" > /etc/kernel/cmdline
 	echo -e "\n# CMDLINE"
 	cat /etc/kernel/cmdline
 
@@ -88,7 +88,10 @@ gentoo_boot() {
 	rm -rf /etc/fstab
 	echo "UUID=$UUID_ESP /efi vfat defaults,noatime,umask=0077 0 1" >> /etc/fstab
 	echo "UUID=$UUID_ROOT / ext4 defaults,noatime 0 2" >> /etc/fstab
-	if (( $TMP_SIZE )); then
+
+	# if 'portage-refresh' has set build to '/tmp' -> make it tmpfs
+	grep -q '^PORTAGE_TMPDIR="/tmp"' /etc/portage/make.conf
+	if (( ! $? )); then
 		echo "tmpfs /tmp tmpfs defaults,mode=1777,size=$TMP_SIZE 0 0" >> /etc/fstab
 	fi
 	echo -e "\n# FSTAB"
@@ -100,35 +103,35 @@ gentoo_finalise() {
 # Finalise by setting up minimal system services, and a user
 
 	# Machine ID & Hostname
-	stat /etc/machine-id > /dev/null
+	stat /etc/machine-id > /dev/null 2>&1
 	if (( $? )); then
 		systemd-machine-id-setup
 	fi
-	echo "gentoo" > /etc/hostname
+	echo "$DEP_HOSTNAME" > /etc/hostname
 
-	# System configurations
-	cp $HOME/dotfile/src/sys/etc/chrony.conf /etc/
+	# Configurations
+	cp "$PATH_DOTFILE/dotfile/src/sys/etc/chrony.conf" /etc/
 
 	mkdir -p /etc/greetd
-	cp $HOME/dotfile/src/sys/etc/greetd/config.toml /etc/greetd/
+	cp "$PATH_DOTFILE/src/sys/etc/greetd/config.toml" /etc/greetd/
 
 
 	# System services/sockets
 	# note: 'systemctl list-unit-files'
 	local SYS_DISABLE=(
-		systemd-networkd.service	# Systemd networking
-		systemd-timesyncd.service       # Systemd ntp
+		systemd-networkd.service     # Systemd networking
+		systemd-timesyncd.service    # Systemd ntp
 	)
 	systemctl disable "${SYS_DISABLE[@]}"
 
 	local SYS_ENABLE=(
 		NetworkManager.service  # Networking
-		chronyd.service	 # NTP
-		greetd.service	  # Login manager
-		cups.service	    # Printing
+		chronyd.service         # NTP
+		greetd.service          # Login manager
+		cups.service            # Printing
 		bluetooth.service       # Bluetooth
-		tlp.service	     # Battery management
-		pcscd.service	   # Smart-card
+		tlp.service             # Battery management
+		pcscd.service           # Smart-card
 		pcscd.socket
 	)
 	systemctl enable "${SYS_ENABLE[@]}"
@@ -137,7 +140,7 @@ gentoo_finalise() {
 	# User services/sockets
 	# note: 'systemctl --user list-unit-files'
 	local USR_DISABLE=(
-		waybar.service	  # Wayland desktop panel service
+		waybar.service    # Wayland desktop panel service
 	)
 	systemctl --global disable "${USR_DISABLE}"
 
@@ -155,41 +158,46 @@ gentoo_finalise() {
 	systemctl --global enable "${USR_ENABLE}"
 
 	# User
-	local USER_NAME="user"
-	local USER_PASS="123"
-	local USER_SHELL="/bin/bash"
-	local USER_GROUPS="$(cat $HOME/dotfile/src/sys/groups)"
+	local USER_GROUPS="$(cat $PATH_DOTFILE/src/sys/groups)"
 
-	id "$USER_NAME" > /dev/null
+	id "$DEP_USER_NAME" > /dev/null
 	if (( $? )); then
-		useradd -m -s $USER_SHELL "$USER_NAME"
+		useradd -m -s $DEP_USER_SHELL "$DEP_USER_NAME"
 		echo "User created"
 	else
 		echo "User exists"
 	fi
-	usermod -aG $USER_GROUPS "$USER_NAME"
-	echo "updated groups" ; groups "$USER_NAME"
+	usermod -aG $DEP_USER_GROUPS "$DEP_USER_NAME"
+	echo "updated groups" ; groups "$DEP_USER_NAME"
 
 	# Reset user password
 	which chpasswd > /dev/null
 	if (( $? )); then
-		echo "chpasswd not in path, set \"$USER_NAME\" password manually!"
+		echo "chpasswd not in path, set \"$DEP_USER_NAME\" password manually!"
 	else
-		echo "$USER_NAME:$USER_PASS" | chpasswd > /dev/null 2>&1
-		echo "credential reset - $USER_NAME:$USER_PASS"
+		echo "$DEP_USER_NAME:$DEP_USER_PASS" | chpasswd > /dev/null 2>&1
+		echo "credential reset - $DEP_USER_NAME:$DEP_USER_PASS"
 		echo "REMEMBER TO CHANGE IT AFTERWARDS!"
 	fi
 
 	# Visudo fragments
 	rm -rf /etc/sudoers.d
 	mkdir -p /etc/sudoers.d
-	cp $HOME/dotfile/src/sys/etc/sudoers.d/* /etc/sudoers.d/
+	cp "$PATH_DOTFILE"/src/sys/etc/sudoers.d/* /etc/sudoers.d/
 	echo ""
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
 	echo -e "GENTOO SETUP (2 of 2)"
 	echo -e "FOLLOW THE INSTRUCTIONS!\n"
+
+	# Dotfile src is required for automatic deployment
+	: "${PATH_DOTFILE:=$HOME/dotfile}"
+	if [[ ! -d "$PATH_DOTFILE" ]]; then
+		echo "missing dotfiles at $PATH_DOTFILE"
+		echo "place them at indicated path, or set custom 'PATH_DOTFILE'"
+		exit 1
+	fi
 
 	gentoo_install
 	gentoo_boot
