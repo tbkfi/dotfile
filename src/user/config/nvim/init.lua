11cff92e5e8e5fe299771b0a8871871f23589d17
Tuomo Björk <tuomo.bjork@tbk.fi>
2025-08-04 00:04:42 +0300

    update: user/config/nvim (filetype tab-settings)

--- src/user/config/nvim/init.lua
+++ src/user/config/nvim/init.lua
@@ -1,9 +1,9 @@
-require("user.settings")	-- General settings
-require("user.mappings")	-- Keymappings
-require("user.plugins")		-- Plugins (Manager)
+require("user.settings")  -- General settings
+require("user.mappings")  -- Keymappings
+require("user.plugins")   -- Plugins (Manager)
 
-require("plugin.treesitter")		-- Syntax Highlighting
-require("plugin.lsp")			-- Language Server Protocol
-require("plugin.cmp")			-- Autocompletion
-require("plugin.vimtex")		-- Latex Tools
-require("plugin.markdown-preview")	-- Markdown Preview
+require("plugin.treesitter")        -- Syntax Highlighting
+require("plugin.lsp")               -- Language Server Protocol
+require("plugin.cmp")               -- Autocompletion
+require("plugin.vimtex")            -- Latex Tools
+require("plugin.markdown-preview")  -- Markdown Preview
--- src/user/config/nvim/lua/user/settings.lua
+++ src/user/config/nvim/lua/user/settings.lua
@@ -3,8 +3,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
 
-vim.opt.textwidth = 80
-vim.opt.colorcolumn = "81"
+vim.opt.textwidth = 90
+vim.opt.colorcolumn = "91"
 
 vim.opt.clipboard:append("unnamedplus")
 
@@ -17,3 +17,31 @@ vim.opt.incsearch = true
 
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+
+-- NO TAB
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "yaml", "toml" },
+	callback = function()
+		vim.opt.expandtab = true
+	end,
+})
+
+-- TAB-4
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "python", "html", "css", "json", "yaml", "toml" },
+	callback = function()
+		vim.bo.tabstop = 4
+		vim.bo.shiftwidth = 4
+	end,
+})
+
+
+-- TAB-2
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "javascript" },
+	callback = function()
+		vim.bo.tabstop = 2
+		vim.bo.shiftwidth = 2
+	end,
+})
